aacbe87af4e48318cc26428e7676021a4885b3ba
Marcus Elizondo-Darwin <61193823+mjwakex@users.noreply.github.com>
2021-11-20 19:13:54 +0100

    did loads of stuffs

--- countdown-timer-static/index.js
+++ countdown-timer-static/index.js
@@ -1,6 +1,6 @@
 const countdown = () => {
   // Specify the date and time we are counting down to.
-  const countDate = new Date("Jan 1, 2022 00:00:00").getTime();
+  const countDate = new Date("Nov 20, 2021, 18:52:00").getTime();
   const now = new Date().getTime();
   const remainingTime = countDate - now;
 
@@ -9,16 +9,26 @@ const countdown = () => {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const textDay = Math.floor(remainingTime / day);
-  const textHour = Math.floor((remainingTime % day) / hour);
-  const textMinute = Math.floor((remainingTime % hour) / minute);
-  const textSecond = Math.floor((remainingTime % minute) / second);
+  var textDay = Math.floor(remainingTime / day);
+  var textHour = Math.floor((remainingTime % day) / hour);
+  var textMinute = Math.floor((remainingTime % hour) / minute);
+  var textSecond = Math.floor((remainingTime % minute) / second);
 
   document.querySelector(".day").innerText = textDay > 0 ? textDay : 0;
   document.querySelector(".hour").innerText = textHour > 0 ? textHour : 0;
   document.querySelector(".minute").innerText = textMinute > 0 ? textMinute : 0;
   document.querySelector(".second").innerText = textSecond > 0 ? textSecond : 0;
+
+  var x = document.getElementById("theme")
+  if (textDay === 0 && textHour === 0 && textMinute === 0 && textSecond == 0){
+    console.log("hello")
+    x.style.display = "block";
+  }  
+
 };
+
+
+
 
 // should use 500 as setInterval won't always run on time.
 setInterval(countdown, 500);
